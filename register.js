1c452f4fbc46021f8132d6e03d82d6141548e977
Wilson Ng <wilsonng234@gmail.com>
2023-05-28 00:05:03 +0800

    Update register.js

--- register.js
+++ register.js
@@ -2,19 +2,72 @@ import dotenv from "dotenv";
 import axios from "axios";
 dotenv.config();
 
-let url = `https://discord.com/api/v8/applications/${process.env.APP_ID}/guilds/${process.env.GUILD_ID}/commands`;
+const globalApplicationCommandsUrl = `https://discord.com/api/v8/applications/${process.env.APP_ID}/commands`;
 
 const headers = {
-    Authorization: `Bot ${process.env.BOT_TOKEN}`,
     "Content-Type": "application/json",
+    Authorization: `Bot ${process.env.BOT_TOKEN}`,
 };
 
-let command_data = {
-    name: "foo",
+const names = ["image", "chat"];
+const descriptions = [
+    "Create an image based on input prompt",
+    "Chat with the bot",
+];
+const options = [
+    [
+        {
+            name: "prompt",
+            description: "Prompt to generate image",
+            type: 3,
+            required: true,
+        },
+        {
+            name: "size",
+            description: "Size of the image",
+            type: 4,
+            required: false,
+            choices: [
+                {
+                    name: "Small",
+                    value: 256,
+                },
+                {
+                    name: "Medium",
+                    value: 512,
+                },
+                {
+                    name: "Large",
+                    value: 1024,
+                },
+            ],
+        },
+    ],
+    [
+        {
+            name: "message",
+            description: "Message to send to the bot",
+            type: 3,
+            required: true,
+        },
+    ],
+];
+
+const jsons = names.map((name, index) => ({
+    name,
     type: 1,
-    description: "replies with bar ;/",
-};
+    description: descriptions[index],
+    options: options[index],
+}));
 
-await axios.post(url, JSON.stringify(command_data), {
-    headers: headers,
-});
+await Promise.all(
+    jsons.map(async (json) => {
+        const strJson = JSON.stringify(json);
+        const config = { headers };
+        await axios.post(globalApplicationCommandsUrl, strJson, config);
+
+        console.log(`Successfully registered command ${json.name}`);
+    })
+);
+
+console.log("Successfully registered all global commands");
