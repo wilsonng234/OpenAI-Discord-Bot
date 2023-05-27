e1dd0d9fcd806719904548b784d38370bbc6ab31
Wilson Ng <wilsonng234@gmail.com>
2023-05-27 23:05:39 +0800

    Use `discord-interactions` package to verifyKey

--- index.js
+++ index.js
@@ -1,44 +1,37 @@
-import nacl from "tweetnacl";
 import dotenv from "dotenv";
 import axios from "axios";
 dotenv.config();
 
+import { verifyKey } from "discord-interactions";
+import { InteractionType, InteractionResponseType } from "discord-interactions";
+
 const handler = async (event) => {
-    // Checking signature (requirement 1.)
-    // Your public key can be found on your application in the Developer Portal
     const PUBLIC_KEY = process.env.PUBLIC_KEY;
-    console.log(PUBLIC_KEY);
     const signature = event.headers["x-signature-ed25519"];
     const timestamp = event.headers["x-signature-timestamp"];
-    const strBody = event.body; // should be string, for successful sign
+    const strBody = event.body;
 
-    const isVerified = nacl.sign.detached.verify(
-        Buffer.from(timestamp + strBody),
-        Buffer.from(signature, "hex"),
-        Buffer.from(PUBLIC_KEY, "hex")
-    );
-
-    if (!isVerified) {
+    const isValidRequest = verifyKey(strBody, signature, timestamp, PUBLIC_KEY);
+    if (!isValidRequest) {
         return {
             statusCode: 401,
-            body: JSON.stringify("invalid request signature"),
+            body: JSON.stringify("Bad request signature"),
         };
     }
 
-    // Replying to ping (requirement 2.)
     const body = JSON.parse(strBody);
-    if (body.type == 1) {
+    if (body.type == InteractionType.PING) {
         return {
             statusCode: 200,
-            body: JSON.stringify({ type: 1 }),
+            body: JSON.stringify({ type: InteractionType.PING }),
         };
     }
 
     const interactions_url = `https://discord.com/api/v10/interactions/${body.id}/${body.token}/callback`;
     await axios.post(interactions_url, {
-        type: 4,
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-            content: "Hello, World.",
+            content: "Hello, World",
         },
     });
 };
