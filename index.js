a7e37e30ed167e5b34d86d5d40f90c1e01667312
Wilson Ng <wilsonng234@gmail.com>
2023-05-28 20:34:14 +0800

    Feat/implement `/image` handler (#2)
    * Install openai
    
    * Pass options to slashCommands handler
    
    * Create `/image` command handler
    
    * Handle image size not provided
    
    * Remove console log

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ const handler = async (event) => {
             body: JSON.stringify({ type: InteractionType.PING }),
         };
     } else {
-        await slashCommands(body.data.name, body.id, body.token);
+        const { id, token, data } = body;
+        const { name, options } = data;
+
+        await slashCommands(name, options, id, token);
     }
 };
 
---
+++ utils/openai/dalle.js
@@ -0,0 +1,23 @@
+import { Configuration, OpenAIApi } from "openai";
+
+async function dalleHandler(prompt, size) {
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    try {
+        const response = await openai.createImage({
+            prompt,
+            n: 1,
+            size: `${size}x${size}`,
+        });
+
+        return response.data.data[0].url;
+    } catch (err) {
+        console.log("dalle error", err);
+        return err.message;
+    }
+}
+
+export default dalleHandler;
