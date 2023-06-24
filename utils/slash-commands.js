1c0350f7bac345fa92b1ef59547bd8b7bd29b608
Wilson Ng <wilsonng234@gmail.com>
2023-06-24 21:05:25 +0800

    Add `/chat` for slashCommands handler

--- utils/slash-commands.js
+++ utils/slash-commands.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { InteractionResponseType } from "discord-interactions";
 
 import dalleHandler from "./openai/dalle.js";
+import chatHandler from "./openai/chat.js";
 
 const slashCommands = async (
     name,
@@ -21,10 +22,26 @@ const slashCommands = async (
                 (option) => option.name === "prompt"
             ).value;
             const sizeValue =
-                options.find((option) => option.name === "size")?.value || "256";
+                options.find((option) => option.name === "size")?.value ||
+                "256";
 
             await axios.patch(patchInteractionUrl, {
                 content: await dalleHandler(promptValue, sizeValue),
+            });
+
+            break;
+        case "chat":
+            console.log("Entered chat");
+            await axios.post(interactionUrl, {
+                type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
+            });
+
+            const messageValue = options.find(
+                (option) => option.name === "message"
+            ).value;
+
+            await axios.patch(patchInteractionUrl, {
+                content: await chatHandler(messageValue),
             });
 
             break;
