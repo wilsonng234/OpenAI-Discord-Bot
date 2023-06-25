89b65c381a4dfe8e42cf3cd81994fc00adb23a8b
Wilson Ng <wilsonng234@gmail.com>
2023-06-25 19:57:58 +0800

    Update register.js to register a channel only

--- register.js
+++ register.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import axios from "axios";
 dotenv.config();
 
-const globalApplicationCommandsUrl = `https://discord.com/api/v8/applications/${process.env.APP_ID}/commands`;
+const globalApplicationCommandsUrl = `https://discord.com/api/v8/applications/${process.env.APP_ID}/guilds/${process.env.GUILD_ID}/commands`;
 
 const headers = {
     "Content-Type": "application/json",
