7ae00c6a5205414e7be5ccdc6a620b0979d645dd
Wilson Ng <wilsonng234@gmail.com>
2023-07-09 18:22:46 +0800

    Write messages to DynamoDB

--- utils/openai/chat.js
+++ utils/openai/chat.js
@@ -1,18 +1,30 @@
+import { writeItem } from "../dynamoDB/writeItem.js";
 import { Configuration, OpenAIApi } from "openai";
 
-async function chatHandler(message) {
+async function chatHandler(message, userId, guildId, channelId) {
     const configuration = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
+    writeItem("Messages", {
+        userId_guildId_channelId: `${userId}_${guildId}_${channelId}`,
+        createTime: new Date().toISOString(),
+        message: message,
+    });
 
     try {
         const response = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [{ role: "user", content: message }],
         });
+        const responseMessage = response.data.choices[0].message.content;
+        writeItem("Messages", {
+            userId_guildId_channelId: `${process.env.APP_ID}_${guildId}_${channelId}`,
+            createTime: new Date().toISOString(),
+            message: responseMessage,
+        });
 
-        return response.data.choices[0].message.content;
+        return responseMessage;
     } catch (err) {
         console.log("chatgpt error", err);
 
