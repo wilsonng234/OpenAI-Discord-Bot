13df364a007c02ebcd79ce0279474a540fa78207
Wilson Ng <wilsonng234@gmail.com>
2023-07-15 13:26:02 +0800

    Replace spread operator with rest operator

--- utils/dynamoDB/writeItem.js
+++ utils/dynamoDB/writeItem.js
@@ -1,11 +1,11 @@
 import docClient from "./dynamoDBDocumentClient.js";
 import { PutCommand } from "@aws-sdk/lib-dynamodb";
 
-const writeItem = async (tableName, item, ...rest) => {
+const writeItem = async (tableName, item, rest) => {
     const command = new PutCommand({
         TableName: tableName,
         Item: item,
-        rest,
+        ...rest,
     });
 
     return await docClient.send(command);
