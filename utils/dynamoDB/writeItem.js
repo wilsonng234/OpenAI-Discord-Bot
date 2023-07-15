8d5f1dcaedaeb39788b2d31422ea1f3f46618c90
Wilson Ng <wilsonng234@gmail.com>
2023-07-15 12:50:07 +0800

    Add rest parameter to writeItem util

--- utils/dynamoDB/writeItem.js
+++ utils/dynamoDB/writeItem.js
@@ -1,10 +1,11 @@
 import docClient from "./dynamoDBDocumentClient.js";
 import { PutCommand } from "@aws-sdk/lib-dynamodb";
 
-const writeItem = async (tableName, item) => {
+const writeItem = async (tableName, item, ...rest) => {
     const command = new PutCommand({
         TableName: tableName,
         Item: item,
+        rest,
     });
 
     return await docClient.send(command);
